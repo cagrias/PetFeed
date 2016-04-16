abc9ec212e1cfc51ce32fa899c6f30aa7200422f
cagrias <aslanbascagri@gmail.com>
2016-04-16 17:10:47 +0300

    apr16

--- app/src/main/java/tr/com/minder/petfeed/settings/SettingsActivity.java
+++ app/src/main/java/tr/com/minder/petfeed/settings/SettingsActivity.java
@@ -14,16 +14,21 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.ViewGroup;
 
 import tr.com.minder.petfeed.R;
+import tr.com.minder.petfeed.google.AppCompatPreferenceActivity;
 
 import java.util.List;
 
@@ -38,137 +43,139 @@ import java.util.List;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class SettingsActivity extends Activity {
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
+public class SettingsActivity extends AppCompatPreferenceActivity {
+//    /**
+//     * A preference value change listener that updates the preference's summary
+//     * to reflect its new value.
+//     */
+//    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+//        @Override
+//        public boolean onPreferenceChange(Preference preference, Object value) {
+//            String stringValue = value.toString();
+//
+//            if (preference instanceof ListPreference) {
+//                // For list preferences, look up the correct display value in
+//                // the preference's 'entries' list.
+//                ListPreference listPreference = (ListPreference) preference;
+//                int index = listPreference.findIndexOfValue(stringValue);
+//
+//                // Set the summary to reflect the new value.
+//                preference.setSummary(
+//                        index >= 0
+//                                ? listPreference.getEntries()[index]
+//                                : null);
+//
+//            } else if (preference instanceof RingtonePreference) {
+//                // For ringtone preferences, look up the correct display value
+//                // using RingtoneManager.
+//                if (TextUtils.isEmpty(stringValue)) {
+//                    // Empty values correspond to 'silent' (no ringtone).
+//                    preference.setSummary(R.string.pref_ringtone_silent);
+//
+//                } else {
+//                    Ringtone ringtone = RingtoneManager.getRingtone(
+//                            preference.getContext(), Uri.parse(stringValue));
+//
+//                    if (ringtone == null) {
+//                        // Clear the summary if there was a lookup error.
+//                        preference.setSummary(null);
+//                    } else {
+//                        // Set the summary to reflect the new ringtone display
+//                        // name.
+//                        String name = ringtone.getTitle(preference.getContext());
+//                        preference.setSummary(name);
+//                    }
+//                }
+//
+//            } else {
+//                // For all other preferences, set the summary to the value's
+//                // simple string representation.
+//                preference.setSummary(stringValue);
+//            }
+//            return true;
+//        }
+//    };
 
-            if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.pref_headers, target);
 
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
+        setContentView(R.layout.settings_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            } else if (preference instanceof RingtonePreference) {
-                // For ringtone preferences, look up the correct display value
-                // using RingtoneManager.
-                if (TextUtils.isEmpty(stringValue)) {
-                    // Empty values correspond to 'silent' (no ringtone).
-                    preference.setSummary(R.string.pref_ringtone_silent);
-
-                } else {
-                    Ringtone ringtone = RingtoneManager.getRingtone(
-                            preference.getContext(), Uri.parse(stringValue));
-
-                    if (ringtone == null) {
-                        // Clear the summary if there was a lookup error.
-                        preference.setSummary(null);
-                    } else {
-                        // Set the summary to reflect the new ringtone display
-                        // name.
-                        String name = ringtone.getTitle(preference.getContext());
-                        preference.setSummary(name);
-                    }
-                }
-
-            } else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
-                preference.setSummary(stringValue);
-            }
-            return true;
-        }
-    };
-
-    /**
-     * Helper method to determine if the device has an extra-large screen. For
-     * example, 10" tablets are extra-large.
-     */
-    private static boolean isXLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-    }
-
-    /**
-     * Binds a preference's summary to its value. More specifically, when the
-     * preference's value is changed, its summary (line of text below the
-     * preference title) is updated to reflect the value. The summary is also
-     * immediately updated upon calling this method. The exact display format is
-     * dependent on the type of preference.
-     *
-     * @see #sBindPreferenceSummaryToValueListener
-     */
-    private static void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-        // Trigger the listener immediately with the preference's
-        // current value.
-        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
+        ActionBar bar = getSupportActionBar();
+        bar.setHomeButtonEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayShowTitleEnabled(true);
+        bar.setHomeAsUpIndicator(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        bar.setTitle(getString(R.string.pref_header_settings));
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
-
-    }
-
-
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            if (!super.onMenuItemSelected(featureId, item)) {
-                NavUtils.navigateUpFromSameTask(this);
-            }
-            return true;
-        }
-        return super.onMenuItemSelected(featureId, item);
-    }
-
-    /**
-     * This method stops fragment injection in malicious applications.
-     * Make sure to deny any unknown fragments here.
-     */
     protected boolean isValidFragment(String fragmentName) {
-        return PreferenceFragment.class.getName().equals(fragmentName)
-                || SettingsFragment.class.getName().equals(fragmentName);
-//                || GeneralPreferenceFragment.class.getName().equals(fragmentName)
-//                || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
-//                || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+        return SettingsFragment.class.getName().equals(fragmentName);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragment {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+//            getActivity().setTheme(R.style.);
 
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.pref_general);
-//            addPreferencesFromResource(R.xml.pref_data_sync);
-//            addPreferencesFromResource(R.xml.pref_headers);
-//            addPreferencesFromResource(R.xml.pref_notification);
+            if (getArguments() != null) {
+                String page = getArguments().getString(getString(R.string.pref_header_settings));
+                if (page != null) {
+                    if (page.equals(getString(R.string.pref_header_general)))
+                        addPreferencesFromResource(R.xml.pref_general);
+                    else if (page.equals(getString(R.string.pref_header_notifications)))
+                        addPreferencesFromResource(R.xml.pref_notification);
+                    else if (page.equals(getString(R.string.pref_header_data_sync)))
+                        addPreferencesFromResource(R.xml.pref_data_sync);
+                }
+            }
+        }
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.settings_page, container, false);
+            if (layout != null) {
+                AppCompatPreferenceActivity activity = (AppCompatPreferenceActivity) getActivity();
+                Toolbar toolbar = (Toolbar) layout.findViewById(R.id.toolbar);
+                activity.setSupportActionBar(toolbar);
+
+                ActionBar bar = activity.getSupportActionBar();
+                bar.setHomeButtonEnabled(true);
+                bar.setDisplayHomeAsUpEnabled(true);
+                bar.setDisplayShowTitleEnabled(true);
+                bar.setHomeAsUpIndicator(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+                bar.setTitle(getPreferenceScreen().getTitle());
+            }
+            return layout;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            if (getView() != null) {
+                View frame = (View) getView().getParent();
+                if (frame != null)
+                    frame.setPadding(0, 0, 0, 0);
+            }
         }
     }
+
 
 //    /**
 //     * This fragment shows general preferences only. It is used when the
